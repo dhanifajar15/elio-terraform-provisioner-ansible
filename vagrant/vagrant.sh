8e55b76039bd55794c926a2e9055699baa2dddf5
dhanifajar15 <dhanifajar15@gmail.com>
2022-02-25 10:15:09 +0700

    modify script

--- vagrant/vagrant.sh
+++ vagrant/vagrant.sh
@@ -1,32 +1,16 @@
+#!/bin/bash
+
 # Install Some Dependencies
-$ sudo apt-get -y install gcc make linux-headers-$(uname -r) dkms
+sudo apt-get -y install gcc make linux-headers-$(uname -r) dkms
 
 # Install Virtual Box
-$ sudo apt install ./virtualbox-5.2_5.2.44-139111~Ubuntu~bionic_amd64.deb
+sudo apt install ./virtualbox-5.2_5.2.44-139111_Ubuntu_bionic_amd64.deb -y
 
 # To Check
-$ VBoxManage -v
-5.2.44r139111
-
------------------------------------------------------------------------------------------------------------------------------
+VBoxManage -v
 
 # Install Extension Pack
-$ sudo VBoxManage extpack install ./Oracle_VM_VirtualBox_Extension_Pack-5.2.44.vbox-extpack
+sudo VBoxManage extpack install ./Oracle_VM_VirtualBox_Extension_Pack-5.2.44.vbox-extpack
 
 # To Check
-$ VBoxManage list extpacks
-Extension Packs: 1
-Pack no. 0:   Oracle VM VirtualBox Extension Pack
-Version:      5.2.44
-Revision:     139111
-Edition:      
-Description:  USB 2.0 and USB 3.0 Host Controller, Host Webcam, VirtualBox RDP, PXE ROM, Disk Encryption, NVMe.
-VRDE Module:  VBoxVRDP
-Usable:       true 
-Why unusable: 
-
------------------------------------------------------------------------------------------------------------------------------
-# Note: Usable: true is mandatory ( Otherwise your virtualbox is not work )
-# Problem: If your system says it has some missing libs such like: libsdl, libvpx etc.
-# Solution: Download packages from https://packages.ubuntu.com/ and Install 
-# Packages Installation ( They are in x.deb format. So, you can install them easily ) 
+VBoxManage list extpacks
